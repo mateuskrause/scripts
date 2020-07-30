75849e6f4707303bb3db71ef82cd224fac351597
mateuskrause <mateus.ke@outlook.com>
2020-07-29 22:55:28 -0300

    Scripts GCloud

---
+++ mountdisk-gcloud.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft && echo mounted.
---
+++ start-mc-server.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sudo screen -S mcs java -Xms1G -Xmx3G -d64 -jar server.jar nogui
